952b59a78745320b5a12198acaccfcb107ae9ab3
baraneldemir <brneldemirrr@gmail.com>
2024-02-05 09:46:46 +0000

    semothing frontend

---
+++ src/utilities/users-service.js
@@ -0,0 +1,8 @@
+import * as usersAPI from  './users-api'
+
+export async function signUp(userData) {
+
+    const token = await usersAPI.signUp(userData)
+    localStorage.setItem('token', token)
+    return getUser()
+}
