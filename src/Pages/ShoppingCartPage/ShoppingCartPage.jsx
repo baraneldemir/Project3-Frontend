1b4af2f0715000c77d160810c306958b52fe1731
Behzad <dr.ben.r.1985@gmail.com>
2024-02-07 10:06:23 +0000

    Shopping Cart Page is Made

--- src/Pages/ShoppingCartPage/ShoppingCartPage.jsx
+++ src/Pages/ShoppingCartPage/ShoppingCartPage.jsx
@@ -19,31 +19,30 @@ export default function ShoppingCartPage({ user }) {
         // eslint-disable-next-line react-hooks/exhaustive-deps
       }, [])
 
-  return (
-    <>
-    <Container className='my-4'>
-      <Stack direction="vertical" gap="2" className="mb-4">
-      <h1 className='me-auto'>Shopping Cart of {user.name}</h1>
-      </Stack>
-      <div style={{
+      return (
+        <>
+          <Container className='my-4'>
+            <Stack direction="vertical" gap="2" className="mb-4">
+              <h1 className='me-auto'>Shopping Cart of {user.name}</h1>
+            </Stack>
+            <div style={{
               display: "grid",
               gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
               gap: "1rem",
               alignItems: "flex-start"
-              }}
-      >{cart && shoppingCart && cart?.products.map( product => {
-        return (
-          <ShoppingCartProductModal
-          key={product.productId._id}
-          name={product.productId.name}
-          image={product.productId.image}
-          price={product.productId.price}
-          />
-        )
-      })}</div>
-
-    </Container>
-    <PayPal />
-    </>
-  )
+            }}>
+              {cart?.products?.map(product => (
+                <ShoppingCartProductModal
+                  key={product.productId._id}
+                  name={product.productId.name}
+                  image={product.productId.image}
+                  price={product.productId.price}
+                />
+              ))}
+            </div>
+          </Container>
+          <PayPal />
+        </>
+      )
+      
 }
--- src/components/ShoppingCartProductModal/ShoppingCartProductModal.jsx
+++ src/components/ShoppingCartProductModal/ShoppingCartProductModal.jsx
@@ -24,19 +24,3 @@ export default function ShoppinCartProductModal({name, image, price}) {
 
 
 
-// import { Card, Button } from "react-bootstrap"
-
-// export default function ShoppinCartProductModal({name, image, price}) {
-//   return (
-//     <Card>
-//       <Card.Img variant="top" src={image} />
-//       <Card.Body>
-//         <Card.Title>{name}</Card.Title>
-//         <Card.Text>{price}</Card.Text>
-//       </Card.Body>
-//       <Card.Footer >
-//         <Button variant="secondary">Buy It Now</Button>
-//       </Card.Footer>
-//     </Card>
-//   )
-// }
