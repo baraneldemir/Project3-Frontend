6e6aa9588c5de3e056834042f0bf926b35456d67
baraneldemir <brneldemirrr@gmail.com>
2024-02-08 13:18:37 +0000

    style changes on slide

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,10 +1,14 @@
 
 import Slide from "../components/Slide/Slide"
 
+import "./HomePage.css"
+
 export default function HomePage() {
   return (
-    <>    
+    <>
+      <div className="slideContainer">    
         <Slide />
+      </div>
     </>
-  )
+  );
 }
--- src/components/Slide/Slide.jsx
+++ src/components/Slide/Slide.jsx
@@ -1,9 +1,10 @@
 import Carousel from 'react-bootstrap/Carousel';
+import '../../pages/HomePage.css'
 
 export default function Slide() {
   return (
-    <Carousel>
-      <Carousel.Item interval={2000}>
+    <Carousel className='slideContainer'>
+      <Carousel.Item interval={4000}>
       <img
           className="d-block w-100"
           src="https://d2r55xnwy6nx47.cloudfront.net/uploads/2020/09/Cosmic_Shear_1200_Social.jpg"
@@ -14,7 +15,7 @@ export default function Slide() {
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item interval={500}>
+      <Carousel.Item interval={4000}>
       <img
           className="d-block w-100"
           src="https://png.pngtree.com/thumb_back/fw800/background/20230722/pngtree-the-vast-expanse-of-the-cosmos-a-3d-rendering-of-celestial-image_3851057.jpg"
@@ -25,7 +26,7 @@ export default function Slide() {
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
+      <Carousel.Item interval={4000}>
       <img
           className="d-block w-100"
           src="https://stsci-opo.org/STScI-01GA6KNV1S3TP2JBPCDT8G826T.png"
