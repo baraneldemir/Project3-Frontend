3ca49cb027602954ab63a9df4e59a6c6e5184c50
Behzad <dr.ben.r.1985@gmail.com>
2024-02-08 14:18:05 +0000

    HomePage

--- src/components/Slide/Slide.jsx
+++ src/components/Slide/Slide.jsx
@@ -9,10 +9,7 @@ export default function Slide() {
           src="https://d2r55xnwy6nx47.cloudfront.net/uploads/2020/09/Cosmic_Shear_1200_Social.jpg"
           alt="First slide"
         />
-        <Carousel.Caption>
-          <h3>Product 1</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
+       
       </Carousel.Item>
       <Carousel.Item interval={500}>
       <img
@@ -20,10 +17,7 @@ export default function Slide() {
           src="https://png.pngtree.com/thumb_back/fw800/background/20230722/pngtree-the-vast-expanse-of-the-cosmos-a-3d-rendering-of-celestial-image_3851057.jpg"
           alt="Second slide"
         />
-        <Carousel.Caption>
-          <h3>Product 2</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
+       
       </Carousel.Item>
       <Carousel.Item>
       <img
@@ -31,12 +25,7 @@ export default function Slide() {
           src="https://stsci-opo.org/STScI-01GA6KNV1S3TP2JBPCDT8G826T.png"
           alt="Third slide"
         />
-        <Carousel.Caption>
-          <h3>Product 3</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
+        
       </Carousel.Item>
     </Carousel>
   );
