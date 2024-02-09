b6bf8d0cbaf8cedd19a358887c29a0a75f8c7176
Behzad <dr.ben.r.1985@gmail.com>
2024-02-09 12:53:49 +0000

    paypal

--- src/PayPal/PayPal.jsx
+++ src/PayPal/PayPal.jsx
@@ -4,15 +4,6 @@ import "./PayPal.css"
 
 
 export default function PayPal() {
-
-  // const initialOptions = {
-  //   "client-id": "test",
-  //   "enable-funding": "",
-  //   "disable-funding": "paylater,venmo,card",
-  //   "data-sdk-integration-source": "integrationbuilder_sc",
-  // };
-  
-
   return (
     <div className="payPal" >
       <PayPalScriptProvider >
